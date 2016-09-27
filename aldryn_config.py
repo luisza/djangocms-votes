f3654c9354d5531cd1313542b3adb80e55ee9ef2
Luis Zárate <luis.zarate@solvosoft.com>
2016-09-27 16:23:51 -0600

    Aldryn market support

---
+++ aldryn_config.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from aldryn_client import forms
+
+
+class Form(forms.BaseForm):
+    comment_notification_email = forms.CharField(
+        'List of notification email (comma separated)',
+        required=True
+    )
+
+    def to_settings(self, data, settings):
+		emails = data.get('comment_notification_email', '')
+        settings['COMMENT_EMAIL_NOTIFICATION'] = emails.split(",")
+        return settings
